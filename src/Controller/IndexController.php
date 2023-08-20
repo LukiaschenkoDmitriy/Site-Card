487378613be20674282ee75cd353928f6693ca02
Dmitriy <lukiaschenkoff@gmail.com>
2023-08-20 04:37:46 +0300

    New chapter 'Languages'

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Language;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Project;
 use App\Entity\Skill;
@@ -16,7 +17,12 @@ class IndexController extends AbstractController
     {
         $skills = $entityManagerInterface->getRepository(Skill::class)->findAll();
         $projects = $entityManagerInterface->getRepository(Project::class)->findAll();
+        $languages = $entityManagerInterface->getRepository(Language::class)->findAll();
 
-        return $this->render("index/index.html.twig", ['skills' => $skills, 'projects' => $projects]);
+        return $this->render("index/index.html.twig", [
+            'skills' => $skills, 
+            'projects' => $projects,
+            "languages" => $languages
+        ]);
     }
 }
---
+++ src/Entity/Language.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LanguageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LanguageRepository::class)]
+class Language
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $rate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRate(): ?int
+    {
+        return $this->rate;
+    }
+
+    public function setRate(int $rate): static
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+}
