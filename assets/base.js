82947f93b844eb64b5fd03ac83f2e11e7dc7c82c
Dmitriy <lukiaschenkoff@gmail.com>
2023-08-23 07:44:26 +0300

    Changed the header view

--- assets/base.js
+++ assets/base.js
@@ -9,3 +9,22 @@
 import "./styles/base.scss";
 import "./styles/contact.scss";
 import "./styles/header.scss";
+
+
+function headerResponsive() {
+    $('.nav_button').on('click', function() {
+        var sub_container = $(".sub_head_container_nav");
+    
+        if (sub_container.css('display') == 'none') sub_container.css('display', 'grid');
+        else sub_container.css('display', 'none');
+    });
+    
+    function maxWidth(x) {
+        $(".sub_head_container_nav").css('display', 'none');
+    }
+    
+    var x = window.matchMedia("(max-width: 600px)");
+    x.addListener(maxWidth);
+}
+
+headerResponsive();
