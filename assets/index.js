1411278cc3e30a63939aa2c49f237a0427a0efff
Dmitriy <lukiaschenkoff@gmail.com>
2023-08-25 20:51:26 +0300

    Changed the chapter 'About me', refactor the chapter 'Home', 'Header'

--- assets/index.js
+++ assets/index.js
@@ -1,2 +1,4 @@
-import './styles/index/index.scss';
 import "./styles/index/home.scss";
+import './styles/index/about.scss';
+import "./styles/index/skills.scss";
+import "./styles/index/languages.scss";
