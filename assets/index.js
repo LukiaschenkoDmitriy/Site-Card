364430ba7236f8582a4bfe8f0ef1ccd31ba16906
Dmitriy <lukiaschenkoff@gmail.com>
2023-08-27 22:04:18 +0300

    Fix the bag with framework.json assets, normalise working typeIt, hljs

--- assets/index.js
+++ assets/index.js
@@ -2,3 +2,18 @@ import "./styles/index/home.scss";
 import './styles/index/about.scss';
 import "./styles/index/skills.scss";
 import "./styles/index/languages.scss";
+
+function homeJavaScript() {
+    hljs.highlightAll()
+    new TypeIt('.php-code code', {
+        speed: 50,
+        waitUntilVisible: true
+    }).go().pause(1000).exec(function() {
+        hljs.highlightAll();
+    });
+};
+
+window.onload = function() {
+    homeJavaScript();
+    console.log("12312");
+};
