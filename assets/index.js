e90e8c0235e5e6fff35c70af7589a89db2371d55
Dmitriy <lukiaschenkoff@gmail.com>
2023-08-30 18:48:55 +0300

    Added the new chapter 'Resume'

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,5 @@
 import "./styles/index/home.scss";
+import './styles/index/resume.scss';
 import './styles/index/about.scss';
 import "./styles/index/skills.scss";
 import "./styles/index/languages.scss";
---
+++ src/Command/query.sql
@@ -0,0 +1,27 @@
+update skill
+set rate = 5
+where name = "C#";
+
+update skill
+set rate = 3
+where name = "Java";
+
+update skill
+set rate = 5
+where name = "Python";
+
+update skill
+set rate = 4
+where name = "JavaScript";
+
+update skill
+set rate = 4
+where name = "TypeScript";
+
+update skill
+set rate = 5
+where name = "NodeJs";
+
+update skill
+set rate = 4
+where name = "PHP";
